ae9cc000004fe52ce5bd5bc655ecdbfb12d49ef9
EnilsonNeto <enilsonneto8@gmail.com>
2022-12-11 11:13:03 -0300

    Acessibilidade do App.tsx ultilizando o 'root'

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Header } from "./components/Header"
 import { GlobalStyle } from './styles/global';
 import Modal from 'react-modal';
 
+Modal.setAppElement('#root');
 export function App() {
   const [isNewTrasactionModalOpen, setIsNewTrasactionModalOpen] = useState(false);
 
