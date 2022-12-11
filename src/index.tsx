1b4d44e802b61cab041d31d467a23d464c553b02
EnilsonNeto <enilsonneto8@gmail.com>
2022-12-11 09:25:10 -0300

    adicionando a biblioteca miragejs ao proejto

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { Server } from 'miragejs';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -8,6 +9,4 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
-
+  </React
