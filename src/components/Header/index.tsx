e6f059dc81cb86ada5960a6bd7ce1dfa1257ee8b
EnilsonNeto <enilsonneto8@gmail.com>
2022-12-11 10:58:02 -0300

    Solução para erro de biblioteca 'react-module'.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,19 @@
-import React from "react"
+import React, { useState } from "react"
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
-
+import Modal from 'react-modal'
 export function Header() {
+    const [isNewTrasactionModalOpen, setIsNewTrasactionModalOpen] = useState(false);
+
+    function handleOpenNewTrasactionModal() {
+        setIsNewTrasactionModalOpen(true);
+    }
+
+    function handleCloseNewTrasactionModal() {
+        setIsNewTrasactionModalOpen(false);
+    }
+
+    
     return (
         <Container>
             <Content>
@@ -10,6 +21,10 @@ export function Header() {
                 <button type="button">
                     Nova Transação
                 </button>
+
+                <Modal isOpen={isNewTrasactionModalOpen}>
+                    <h2>Cadastrar Transação</h2>
+                </Modal>
            </Content>
         </Container>
     )
